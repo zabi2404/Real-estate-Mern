36088628fd60b10e31b8129db7986e6494f95158
Zohaib Ali <zohaib24a@gmail.com>
2026-04-06 02:36:29 +0500

    fix dev

--- real-estate/src/main.tsx
+++ real-estate/src/main.tsx
@@ -8,6 +8,7 @@ import { persistor } from './redux/store.ts'
 import axios from 'axios'
 
 axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL || '';
+axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById('root')!).render(
 
